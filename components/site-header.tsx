d33beda6bbf7974bb8681db628bccb7297b9cf47
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-13 17:34:19 +0000

    .

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -27,7 +27,7 @@ export function SiteHeader() {
 
   return (
     <header className="bg-background sticky top-0 z-40 w-full">
-      <div className="container px-4 pr-8 flex h-16 items-center space-x-4 sm:justify-between sm:space-x-1">
+      <div className="container px-4 pr-8 flex h-16 items-center space-x-4 sm:justify-between">
        <MobileNav/>    
      <MainNav/>   
         <div className="flex flex-1 items-center justify-end space-x-4">
