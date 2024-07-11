f75dab75ff8d1737526c1d19a2d01104fbb77b36
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-11 10:39:30 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -161,7 +161,7 @@ export default function RootLayout({ children, metadata: Metadata}: RootLayoutPr
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <head meta={metadata}/>
+      <head/>
       <body
         className={`min-h-screen bg-background font-sans antialiased ${fontSans.variable}`}
       >
