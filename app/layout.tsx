c49181e8ed20c8447d5463bcc39307fba6350c72
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-13 06:19:32 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-"use client"
 
 import "@/styles/globals.css"
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
+import { WagmiRainbowProvider } from '@/context/WagmiRainbowContext'
 import { PropsWithChildren } from 'react';
 import { siteConfig } from '@/config/site'
 import { headers } from "next/headers";
@@ -31,43 +31,102 @@ interface RootLayoutProps {
   
 }
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-    sepolia,
-    bscTestnet,
-    mainnet,
-    {
-      ...zetachainAthensTestnet,
-      iconUrl: "https://www.zetachain.com/favicon/favicon.png",
-    },
-  ],
-  [alchemyProvider({ apiKey: `${process.env.ALCHEMY_API_KEY}`}), publicProvider()]
-)
-
-const connectors = connectorsForWallets([
-  {
-    groupName: "Recommended",
-    wallets: [
-      injectedWallet({ chains }),
-      metaMaskWallet({ projectId: "PROJECT_ID", chains }),
-      xdefiWallet({ chains }),
-      okxWallet({ projectId: "PROJECT_ID", chains }),
-    ],
-  },
-])
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-  webSocketPublicClient,
-})
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  metadataBase: new URL('https://defione.io'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'de-DE': '/de-DE',
+      'es-ES': '/es-ES',
+      'fr-FR': '/fr-FR',
+      'jp-JP': '/jp-JP',
+      'ko-KO': '/ko-KP',
+      'zh-ZH': '/zh-ZH',
+      'pt-PT': '/pt-PT',
+    },
+  },
+  description: siteConfig.description,
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Wyoming DAO', 'DAO', 'Quantum One DAO', 'Quantum One', 'DA-FI Token', 'Web 3', 'Robert Mourey Jr', 'Cody Clark', 'Sui', 'SAFE', 'smart accounts', 'DAO airdrop', 'DeFi', 'DEX', 'BitCoin', 'DeFi ONE', 'new opportunity paradigm', 'DAO owned data centers', 'quantum blockchain', 'Ethereum', 'DeFi', 'omni-chain defi','BitCoin smart contracts', 'ERC20', 'governance token', 'DEX governance token', 'open blockchain network', 'digital consumer assets', 'Wyoming DAO', 'Ethereum', 'DeFi Llama', 'swap token and nfts', 'swap on any chain', 'BitCoin DeFi', 'engineering opportunity', 'token lock campaign', 'Zetachain', 'SAFE', 'quantum based blockchain networks', 'decentralized opportunity', 'Da-Fi Token', 'wallet connect', 'github', 'Uniswap v3 smart contracts', 'TVL', 'post quantum cryptography', 'post quantum cryptocurrency', 'decentralized ai', 'blockchain ai', 'ai', 'decentralized compliance protocols'],
+  authors: [{ name: 'Quantum One DAO' }],
+  creator: 'Quantum One DAO',
+  publisher: 'DeFi One', 
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  generator: 'NextJS',
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    url: "https://defione.io",
+    images: [
+      {
+        url: 'https://defione.io/og-image.jpg', // Must be an absolute URL
+        width: 1230,
+        height: 640,
+      },
+      {
+        url: 'https://quantumone.b-cdn.net/onyx/opengraph-image.jpg', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'blockchain business',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+    twitter: {
+         title: siteConfig.name,
+         description: siteConfig.description,
+         site: '@dfione',
+         creator: '@dfione',
+         images: [
+      {
+        url: 'https://defione.io/twitter-image.jpg', // Must be an absolute URL
+        width: 1800,
+        height: 900,
+      },
+      {
+        url: 'https://quantumone.b-cdn.net/onyx/twitter-image.jpg',
+        width: 1800,
+        height: 900,
+      },
+     ],
+   },
+}
 
 
 export default function RootLayout({ children}: RootLayoutProps) {
@@ -78,20 +137,12 @@ export default function RootLayout({ children}: RootLayoutProps) {
       <body
         className={`min-h-screen bg-background font-sans antialiased ${fontSans.variable}`}
       >
-        <WagmiConfig config={wagmiConfig}>
-          <RainbowKitProvider 
-        theme={midnightTheme({
-        accentColor: '#6b7280',
-        accentColorForeground: 'white',
-        borderRadius: 'medium',
-        fontStack: 'system',
-      })}
-      chains={chains}>
+        <WagmiRainbowProvider
+      
             <ZetaChainProvider>
               <Index>{children}</Index>
             </ZetaChainProvider>
-          </RainbowKitProvider>
-        </WagmiConfig>
+          </WagmiRainbowProvider/>
       </body>
     </html>
   )
