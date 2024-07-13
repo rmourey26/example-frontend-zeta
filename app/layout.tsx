535d3119f367dd1797df533d32e5132450981b1a
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-13 06:29:59 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,7 +135,7 @@ export default function RootLayout({ children}: RootLayoutProps) {
       <body
         className={`min-h-screen bg-background font-sans antialiased ${fontSans.variable}`}
       >
-        <WagmiRainbowProvider
+        <WagmiRainbowProvider>
       
            
               <Index>{children}</Index>
