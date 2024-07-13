1fe0b9a7f7860bcbe6aac192fd090f42bbde3088
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-13 06:33:56 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -131,10 +131,8 @@ export default function RootLayout({ children}: RootLayoutProps) {
 
   return (
     <html lang="en" suppressHydrationWarning>
-      
       <body
-        className={`min-h-screen bg-background font-sans antialiased ${fontSans.variable}`}
-      >
+        className={`min-h-screen bg-background font-sans antialiased ${fontSans.variable}`}>
         <WagmiRainbowProvider>
       
            
